d3d330655f271e18172cae1d32ee2ed2d1236f57
bznhkka <gab.sombria@gmail.com>
2023-06-20 14:38:37 +0800

    animation script + environment
    animation script updated WASD Arrow Keys Controls
    Walls, Floor and Lava added to Environment
    2D Collider Added

--- Assets/Dungeon Knight/SCRIPT/playermovement.cs
+++ Assets/Dungeon Knight/SCRIPT/playermovement.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
+    //Speed
+    public float moveSpeed;
+
+    //RigidBody
+    public Rigidbody2D rigidBody;
+    private Vector2 movementInput;
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic WASD Movement of Player
+        //W
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.enabled = true;
+            anim.SetTrigger("BackAni");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.enabled = false;
+        }
+
+        //S
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.enabled = true;
+            anim.SetTrigger("ForwardAni");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.enabled = false;
+        }
+        //A
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.enabled = true;
+            anim.SetTrigger("LeftAni");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.enabled= false;
+        }
+        //D
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.enabled = true;
+            anim.SetTrigger("RightAni");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.enabled = false;
+        }
+
     }
+    // best for physics calculations
+    private void FixedUpdate()
+    {
+        //makes out player move
+        rigidBody.velocity = movementInput * moveSpeed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        //WASD = vector 2 nvalues
+        movementInput = inputValue.Get<Vector2>();
+    }
+
 }
+
